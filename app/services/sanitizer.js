6a246b5539b0f1992abfa6acca2ede6494c885c7
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-07-09 07:22:03 +0200

    build: :wrench: config sanitizer

---
+++ app/services/sanitizer.js
@@ -0,0 +1,12 @@
+const sanitizer = require('sanitizer');
+
+const bodySanitizer = (req, res, next) => {
+  if (req.body) {
+    Object.keys(req.body).forEach((propName) => {
+      req.body[propName] = sanitizer.escape(req.body[propName]);
+    });
+  }
+  next();
+};
+
+module.exports = bodySanitizer;
