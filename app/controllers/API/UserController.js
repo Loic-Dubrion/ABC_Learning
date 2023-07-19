50e9bc176827473c9d8f41abafd2cc86505027aa
Loic-Dubrion <cochisecoicoin@gmail.com>
2023-07-19 20:10:21 +0200

    fix: :bug: hash password

--- app/controllers/API/UserController.js
+++ app/controllers/API/UserController.js
@@ -33,69 +33,40 @@ class UserController extends CoreController {
     response.status(201).json(results);
   }
 
-  //   /**
-  //    * Update an existing user.
-  //    *
-  //    * @param {Object} request - 1, 2 or 3 parameters (username, email, password)
-  //    * and the actualy password.
-  //    * @param {Object} response - The response object.
-  //    */
-  //   async updateUser(request, response) {
-  //     const { userId } = request.params;
-  //     const { ...objData } = request.body;
+  async updateUser(request, response) {
+    const { userId } = request.params;
+    const { ...objData } = request.body;
 
-  //     let user = await this.constructor.dataMapper.findOneByField('id', userId);
-  //     const isGoodPassword = await bcrypt.compare(objData.old_password, user.password);
-  //     if (!isGoodPassword) {
-  //       throw new Error400('Password invalid');
-  //     }
+    let user = await this.dataMapper.findOneByField('id', userId);
+    const isGoodPassword = await bcrypt.compare(objData.old_password, user.password);
+    if (!isGoodPassword) {
+      throw new Error400('Password invalid');
+    }
 
-  //     // if new password hash
-  //     if (objData.password) {
-  //       const hashedPassword = await bcrypt.hash(objData.password, 10);
-  //       objData.password = hashedPassword;
-  //     }
+    // if new password hash
+    if (objData.password) {
+      const hashedPassword = await bcrypt.hash(objData.password, 10);
+      objData.password = hashedPassword;
+    }
 
-  //     const results = await this.constructor.dataMapper.executeFunction('update_user', userId, objData);
+    const results = await this.dataMapper.executeFunction('update_user', userId, objData);
 
-  //     // Update user's data with new values
-  //     user = { ...user, ...objData };
+    // Update user's data with new values
+    user = { ...user, ...objData };
 
-  //     // Get roles and permissions of the user
-  //     const rolesAndPermissions = await auth.getUserRolesAndPermissions(userId);
-  //     user = { ...user, ...rolesAndPermissions };
+    // Get roles and permissions of the user
+    // const rolesAndPermissions = await auth.getUserRolesAndPermissions(userId);
+    // user = { ...user, ...rolesAndPermissions };
 
-  //     // generate access and refresh tokens after user details update
-  //     const accessToken = auth.generateAccessToken(request.ip, user);
-  //     const refreshToken = await auth.generateRefreshToken(user);
+    // generate access and refresh tokens after user details update
+    // const accessToken = auth.generateAccessToken(request.ip, user);
+    // const refreshToken = await auth.generateRefreshToken(user);
 
-  //     response.json({
-  //       update_user: results[0].update_user,
-  //       tokens: { accessToken, refreshToken },
-  //     });
-  //   }
-
-  //   /**
-  //    * Delete a user.
-  //    *
-  //    * @param {Object} request - User Id in the URL.
-  //    * @param {Object} response - The response object.
-  //    */
-  //   async deleteUser(request, response) {
-  //     const { userId } = request.params;
-  //     const { password } = request.body;
-
-  //     const user = await this.constructor.dataMapper.findOneByField('id', userId);
-
-  //     const isGoodPassword = await bcrypt.compare(password, user.password);
-  //     if (!isGoodPassword) {
-  //       throw new Error400('Password invalid');
-  //     }
-
-//     const results = await this.constructor.dataMapper.executeFunction('delete_user', userId);
-//     response.json(results);
-//   }
-// }
+    response.json({
+      update_user: results[0].update_user,
+      // tokens: { accessToken, refreshToken },
+    });
+  }
 }
 
 module.exports = new UserController();
